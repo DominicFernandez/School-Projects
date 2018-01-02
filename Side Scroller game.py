16dd3825f9159b258b755f7588b4c2c7ab202a6b
DJ <dominicfernandez@students.berkeley.net>
2018-01-02 15:28:37 -0800

    Side scrolling game with pygame

--- Side Scroller game.py
+++ Side Scroller game.py
@@ -7,16 +7,43 @@ WIDTH = 360
 HEIGHT = 480
 FPS = 30
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+
 pygame.init() # starts the game
 pygame.mixer.init() # used for sound and music
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Screen Scroller")
 clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 #Game loop
 running = True
 while running:
+    clock.tick(FPS)
     #Process input (events)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
     #Update
+    all_sprites.update()
     #Draw / render
-    screen.fill(0, 255, 255)
+    screen.fill(CYAN)
+    all_sprites.draw(screen)
+    pygame.display.flip()
